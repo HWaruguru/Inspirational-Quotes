e10b093c3b805db6d4717cc8baebb04465d83985
Waruguru <hannah.waruguru8@gmail.com>
2021-08-20 21:47:49 +0300

    remove the category property

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quotes } from '../quotes';
+import { Quote } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -8,9 +8,14 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   title = 'Welcome to Inspirational Quotes';
-  quotes: Quotes [] = [
-    new Quotes(1, 'Hannah', 'Insipirational','Be yourself; everyone else is already taken.', '― Oscar Wilde', new Date(2020,8,20), 0, 0)
-  ]
+  quotes: Quote[] = [
+    new Quote(1, 'Hannah', 'Be yourself; everyone else is already taken.', '― Oscar Wilde', new Date(2020,8,20), 0, 0),
+    new Quote(2, 'Waruguru', 'You\'ve gotta dance like there\'s nobody watching,Love like you\'ll never be hurt, Sing like there\'s nobody listening, And live like it\'s heaven on earth.', '― William W. Purkey', new Date(2020,8,20), 0, 0),
+    new Quote(3, 'Gathu', 'Live as if you were to die tomorrow. Learn as if you were to live forever.', '― Mahatma Gandhi', new Date(2020,8,20), 0, 0),
+    new Quote(4, 'Kieya', 'The most wasted of all days is one without laughter.', '― Nicolas Chamfort', new Date(2020,8,20), 0, 0),
+    new Quote(5, 'Peace', 'I like the night. Without the dark, we\'d never see the stars.', '― Stephenie Meyer, Twilight', new Date(2020,8,20), 0, 0),
+    new Quote(6, 'Wanyenze', 'And now these three remain: faith, hope and love. But the greatest of these is love.', '― The Holy Bible', new Date(2020,8,20), 0, 0),
+  ];
 
   constructor() { }
 
